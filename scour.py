40f02e6fb7786612de583cc0ba4e4c41106225a6
Jan Hecking <jan@travelmob.com>
2015-01-09 14:54:10 +0800

    add Sketch NS

--- scour.py
+++ scour.py
@@ -86,13 +86,15 @@ NS = { 	'SVG': 		'http://www.w3.org/2000/svg',
 		'ADOBE_FLOWS': 'http://ns.adobe.com/Flows/1.0/',
 		'ADOBE_IMAGE_REPLACEMENT': 'http://ns.adobe.com/ImageReplacement/1.0/',     
 		'ADOBE_CUSTOM': 'http://ns.adobe.com/GenericCustomNamespace/1.0/',
-		'ADOBE_XPATH': 'http://ns.adobe.com/XPath/1.0/'
+		'ADOBE_XPATH': 'http://ns.adobe.com/XPath/1.0/',
+		'SKETCH': 'http://www.bohemiancoding.com/sketch/ns'
 		}
 
 unwanted_ns = [ NS['SODIPODI'], NS['INKSCAPE'], NS['ADOBE_ILLUSTRATOR'],
 				NS['ADOBE_GRAPHS'], NS['ADOBE_SVG_VIEWER'], NS['ADOBE_VARIABLES'],
 				NS['ADOBE_SFW'], NS['ADOBE_EXTENSIBILITY'], NS['ADOBE_FLOWS'],
-				NS['ADOBE_IMAGE_REPLACEMENT'], NS['ADOBE_CUSTOM'], NS['ADOBE_XPATH'] ] 
+				NS['ADOBE_IMAGE_REPLACEMENT'], NS['ADOBE_CUSTOM'], NS['ADOBE_XPATH'],
+				NS['SKETCH'] ]
 
 svgAttributes = [
 				'clip-rule',
